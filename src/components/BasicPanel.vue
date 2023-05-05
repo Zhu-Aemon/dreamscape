0dcb801695f207d4eba10a0fa1b2c95119942e9c
TianZhu_Wang <stephen_einstein@iCloud.com>
2023-05-05 12:45:31 +0800

    init

--- src/components/BasicPanel.vue
+++ src/components/BasicPanel.vue
@@ -111,21 +111,48 @@
       </div>
     </div>
   </div>
-  <canvas id="myChart"></canvas>
+  <div ref="chartContainer">
+  </div>
 </template>
 
 <script setup>
 import axios from "axios";
-import { OhlcElement, OhlcController, CandlestickElement, CandlestickController } from 'chartjs-chart-financial'
-import Chart from 'chart.js/auto' // Easy way of importing everything
-import 'chartjs-adapter-date-fns';
-import { onMounted, ref, shallowRef } from "vue";
+import { onMounted, ref, shallowRef, onUnmounted } from "vue";
+import { createChart } from "lightweight-charts";
 
-const chart = shallowRef(null)
+let chart
+const chartContainer = ref()
 
 onMounted(() => {
-  Chart.register(OhlcElement, OhlcController, CandlestickElement, CandlestickController)
+  const chartOptions = { layout: { textColor: 'black', background: { type: 'solid', color: 'white' } } };
+  chart = createChart(chartContainer.value, chartOptions)
+  const candleSeries = chart.addCandlestickSeries({
+    upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
+    wickUpColor: '#26a69a', wickDownColor: '#ef5350',
+  })
+
+  candleSeries.setData([
+    { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
+    { time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
+    { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
+    { time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
+    { time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
+    { time: '2018-12-27', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
+    { time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
+    { time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
+    { time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
+    { time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
+  ]);
+
+  // chart.timeScale().fitContent()
 })
+
+onUnmounted(() => {
+  if (chart) {
+    chart.remove();
+    chart = null;
+  }
+});
 
 const getStockData = async (stock) => {
   const response = await axios.get(`http://localhost:3001/stock?name=AAPL`)
